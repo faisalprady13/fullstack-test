7bf6858dfcf48713830d777a29748ff599378cef
Faisal Prady <faisalprady@faisals-mbp.localdomain>
2024-10-28 12:38:10 +0100

    wip

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,18 +1,21 @@
 import React, { useEffect, useState } from "react";
+import { Product } from "@shared/types";
 
 const App: React.FC = () => {
-  const [message, setMessage] = useState("");
+  const [products, setProducts] = useState<Product[]>([]);
 
   useEffect(() => {
-    fetch("http://localhost:3000/api/hello")
+    fetch("http://localhost:3000/api/products")
       .then((res) => res.json())
-      .then((data) => setMessage(data.message))
+      .then((data) => {
+        setProducts(data);
+      })
       .catch((err) => console.error(err));
   }, []);
 
   return (
     <div>
-      <h1>{message ? message : "Loading..."}</h1>
+      <h1>{products[0] ? products[0].name : "Loading..."}</h1>
     </div>
   );
 };
--- server/src/server.ts
+++ server/src/server.ts
@@ -1,5 +1,21 @@
 import express, { Express, Request, Response } from "express";
 import cors from "cors";
+import { Product } from "@shared/types";
+
+const data: Product[] = [
+  {
+    id: 1,
+    name: "Product A",
+    description: "Product A description",
+    price: 20,
+  },
+  {
+    id: 2,
+    name: "Product B",
+    description: "Product B description",
+    price: 30,
+  },
+];
 
 const app: Express = express();
 const PORT = process.env.PORT || 3000;
@@ -8,6 +24,10 @@ app.use(cors());
 
 app.get("/api/hello", (req: Request, res: Response) => {
   res.json({ message: "Hello from Flink!" });
+});
+
+app.get("/api/products", (req: Request, res: Response) => {
+  res.json(data);
 });
 
 app.listen(PORT, () => {
